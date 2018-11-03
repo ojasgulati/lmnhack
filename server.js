60ee48cec9444dca9573f8cde375361604ef9422
ojasgulati <ojasgulati100@gmail.com>
2018-11-04 04:43:36 +0530

    1st

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const socketIO = require("socket.io");
+const http = require("http");
+
+const app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+app.use(bodyParser.json());
+
+io.on("connection", socket => {
+  console.log("User connected");
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  app.get("/python", (req, res) => {
+    console.log("ojas");
+    socket.emit("python", "true");
+  });
+});
+
+server.listen(80, () => {
+  console.log(`Server running on port 80`);
+});
